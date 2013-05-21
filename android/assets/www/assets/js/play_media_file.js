93552e60a9d48183336faeff42014c8a70a91e3a
root <root@eb97dc58-49a1-47b3-a428-96df4ec99c66>
2013-05-21 13:00:06 +0000

    autocommit

--- android/assets/www/assets/js/play_media_file.js
+++ android/assets/www/assets/js/play_media_file.js
@@ -1,12 +1,85 @@
-function  play_file(file) {
+var current_position = 0;
+var tmp_pos = 0;
+
+function  load_media_file(file) {
 	console.log(file);
 	my_media = new Media(file,
         function() {
-            alert("playAudio():Audio Success");
+            alert("Audio Success");
+			
         },
             function(err) {
                 alert(err);
         }
-        );
-      my_media.play();
+    ); 
+	
+	play();
+}
+
+function play() {
+	console.log(media_timer);
+	console.log(my_media.getDuration());
+	console.log('position '+current_position);
+	if (my_media != null) {
+		
+		if (current_position > 0) {
+			console.log("seek "+current_position*1000);
+			my_media.seekTo(current_position*1000);
+			console.log(my_media);
+		}
+		
+		my_media.play();
+		
+		if (media_timer == null) {
+			media_timer = setInterval(function() {
+				// get my_media position
+				my_media.getCurrentPosition(
+					// success callback
+					function(position) {
+						if (position > -1) {
+							setAudioPosition((position));
+						}
+					},
+					// error callback
+					function(e) {
+						console.log("Error getting pos=" + e);
+						setAudioPosition("Error: " + e);
+					}
+				);
+			}, 1000);
+		}
+	} else {
+		play_location_sound();
+	}
+}
+
+function stop() {
+	if (my_media != null) {
+		my_media.pause();
+		setAudioPosition(0);
+		my_media.seekTo(0);
+		
+		current_position  = 0;
+	} else {
+		play_location_sound();
+	}
+}
+
+function pause() {
+	console.log('position '+current_position);
+	if (my_media != null) {
+		my_media.pause();
+	} else {
+		play_location_sound();
+	}
+}
+
+function setAudioPosition(position) {
+	if (tmp_pos != position) {
+		tmp_pos = position;
+		current_position = current_position+1;
+	}
+
+	$("#time-slider").val(current_position);
+	$("#time-slider").slider("refresh");
 }
