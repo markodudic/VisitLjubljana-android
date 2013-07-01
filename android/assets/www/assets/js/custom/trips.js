4f4abb9deabf115ed0aac902d65ca3658aad3852
Marko Dudić <marko.dudic@gmail.com>
2013-07-01 13:56:37 +0000

    marko: popravki

--- android/assets/www/assets/js/custom/trips.js
+++ android/assets/www/assets/js/custom/trips.js
@@ -15,22 +15,6 @@ function load_trip_content(id, transition, reverse, save_history) {
 	generate_query(tmp_query, tmp_callback);
 }
 
-function load_voice_guide(save_history) {
-	console.log('load_voice_guide');
-	if (save_history == 1)  {
-		var history_string = "fun--load_voice_guide--empty";
-		add_to_history(history_string);
-	}
-
-
-	voice_guide = 1;
-	swipe		= 0;
-
-	var tmp_query 		= "SELECT zp.*, zpt.title, zcg.id_group FROM ztl_poi zp LEFT JOIN ztl_poi_category zpc ON zpc.id_poi = zp.id LEFT JOIN ztl_category_group zcg ON zcg.id_category = zpc.id_category LEFT JOIN ztl_poi_translation zpt ON zpt.id_poi = zp.id WHERE zpt.id_language = "+settings.id_lang+" AND sound != '' GROUP BY zp.id";
-	var tmp_callback	= "load_pois_success";
-	
-	generate_query(tmp_query, tmp_callback);
-}
 
 function play_voice_guide(id) {
 	console.log('play_voice_guide');
