9f1b1e1884bd2593501c2578d03b33ed1be6f791
lili <lili@eb97dc58-49a1-47b3-a428-96df4ec99c66>
2013-07-05 10:57:28 +0000

    poi description fix

--- android/assets/www/assets/js/custom/load_settings.js
+++ android/assets/www/assets/js/custom/load_settings.js
@@ -70,7 +70,7 @@ function load_trip_content(id, transition, reverse, save_history) {
 	trip_id 	= id;
 	sound_file 	= "";
 
-	var tmp_query 		= 'SELECT zp.*, zpt.title, zcg.id_group FROM ztl_poi zp LEFT JOIN ztl_poi_category zpc ON zpc.id_poi = zp.id LEFT JOIN ztl_category_group zcg ON zcg.id_category = zpc.id_category LEFT JOIN ztl_poi_translation zpt ON zpt.id_poi = zp.id WHERE zp.id = '+id+' AND zpt.id_language = '+settings.id_lang+' GROUP BY zp.id';
+	var tmp_query 		= 'SELECT zp.*, zpt.title, zpt.description, zcg.id_group FROM ztl_poi zp LEFT JOIN ztl_poi_category zpc ON zpc.id_poi = zp.id LEFT JOIN ztl_category_group zcg ON zcg.id_category = zpc.id_category LEFT JOIN ztl_poi_translation zpt ON zpt.id_poi = zp.id WHERE zp.id = '+id+' AND zpt.id_language = '+settings.id_lang+' GROUP BY zp.id';
 	var tmp_callback	= "load_poi_success";
 
 	generate_query(tmp_query, tmp_callback);
