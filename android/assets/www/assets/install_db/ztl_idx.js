de7ed430898560430f1cd68db0bc090b751412fc
lili <lili@eb97dc58-49a1-47b3-a428-96df4ec99c66>
2013-07-05 07:03:14 +0000

    --ocitno mi ne commita novih slik // bom dal folder do uploads na ftp -- posljem link v mejlu

--- android/assets/www/assets/install_db/ztl_idx.js
+++ android/assets/www/assets/install_db/ztl_idx.js
@@ -28,25 +28,4 @@ function populateDB_ztl_tour_images(tx) {
 	tx.executeSql('CREATE INDEX ztl_tour_translation__id_language ON ztl_tour_translation (id_language ASC)');
 	tx.executeSql('CREATE INDEX ztl_tour_images__id_tour ON ztl_tour_images (id_tour ASC)');
 	tx.executeSql('CREATE INDEX ztl_tour_chaters__id_tour ON ztl_tour_chaters (id_tour ASC)');
-	
-	tx.executeSql('CREATE UNIQUE INDEX ztl_group__u ON ztl_group (id, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_category__u ON ztl_group (id, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_category_group__u ON ztl_category_group (id_category, id_group);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_poi__u ON ztl_poi (id);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_poi_translation__u ON ztl_poi_translation (id_poi, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_poi_category__u ON ztl_poi_category (id_poi, id_category);');
-
-	tx.executeSql('CREATE UNIQUE INDEX ztl_event__u ON ztl_event (id);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_event_translation__u ON ztl_event_translation (id_event, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_event_category__u ON ztl_event_category (id, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_event_event_category__u ON ztl_event_event_category (id_event, id_event_category);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_event_timetable__u ON ztl_event_timetable (id_event, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_event_pricing__u ON ztl_event_pricing (id_event, id_language);');
-	
-	tx.executeSql('CREATE UNIQUE INDEX ztl_tour__u ON ztl_tour (id);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_tour_translation__u ON ztl_tour_translation (id_tour, id_language);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_tour_chaters__u ON ztl_tour_chaters (id_tour, id_language, tour_idx);');
-	tx.executeSql('CREATE UNIQUE INDEX ztl_tour_images__u ON ztl_tour_images (id_tour, tour_idx);');
-	
-	tx.executeSql('CREATE UNIQUE INDEX ztl_info__u ON ztl_info (id, id_language);');
 }
--- android/assets/www/assets/install_db/ztl_tour.js
+++ android/assets/www/assets/install_db/ztl_tour.js
@@ -1,4 +1,4 @@
 function populateDB_ztl_tour(tx) {
 	tx.executeSql('DROP TABLE IF EXISTS ztl_tour');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS ztl_tour (id,turisticna_kartica,validity_from,validity_to,record_status)');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS ztl_tour (id,turisticna_kartica,validity_from,validity_to, record_status)');
 }
