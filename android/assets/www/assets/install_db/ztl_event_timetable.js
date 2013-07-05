2a9bff0dc8f7177a6e6a619107ee346b75d0c8c1
lili <lili@eb97dc58-49a1-47b3-a428-96df4ec99c66>
2013-07-05 07:18:36 +0000

    nadgradnja tabel

--- android/assets/www/assets/install_db/ztl_event_timetable.js
+++ android/assets/www/assets/install_db/ztl_event_timetable.js
@@ -1,4 +1,4 @@
 function populateDB_ztl_event_timetable(tx) {
 	tx.executeSql('DROP TABLE IF EXISTS ztl_event_timetable');
-	tx.executeSql('CREATE TABLE IF NOT EXISTS ztl_event_timetable (id_event,venue_id,venue,date)');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS ztl_event_timetable (id_event,id_language,venue_id,venue,date,date_first,date_last)');
 }
