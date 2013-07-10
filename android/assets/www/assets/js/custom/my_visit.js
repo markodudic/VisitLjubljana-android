84663b7c1e668cced241d8a56f15b7d6aba56c70
Marko Dudić <marko.dudic@gmail.com>
2013-07-10 11:00:06 +0000

    myvisit seznam + link na izbran objekt

--- android/assets/www/assets/js/custom/my_visit.js
+++ android/assets/www/assets/js/custom/my_visit.js
@@ -1,9 +1,4 @@
 function add_to_my_visit(id, type, time, location) {
-	console.log("my_visit --- "+ id);
-	console.log("my_visit --- "+ type);
-	console.log("my_visit --- "+ time);
-	console.log("my_visit --- "+ location);
-
 	var tmp_query      = "INSERT OR REPLACE INTO ztl_my_visit (id, main_group, type, time, location) VALUES ("+id+", "+selected_group+", "+type+", "+time+", "+location+");";
     db.transaction(function(tx) {
 		 tx.executeSql(tmp_query, [], function(tx, res) {});
@@ -11,14 +6,19 @@ function add_to_my_visit(id, type, time, location) {
 }
 
 function load_my_visit() {
-	console.log("my_visit --- load");
-
-	var tmp_query      = "SELECT * FROM ztl_my_visit ORDER BY type, main_group";
+	var tmp_query      = "SELECT id, main_group, type, time, location FROM ztl_my_visit GROUP BY id, main_group, type, time, location ORDER BY type, main_group";
     var tmp_callback   = "my_visit_success";
 
     generate_query(tmp_query, tmp_callback);
 }
 
-function delete_from_my_visit() {
-	console.log("my_visit --- delete");
+function delete_from_my_visit(id, time) {
+	if (time == -1) {
+		var tmp_query = "DELETE FROM ztl_my_visit WHERE id = "+id;
+	} else {
+		var tmp_query = "DELETE FROM ztl_my_visit WHERE id = "+id+" AND time = "+time;
+	}
+	
+    var tmp_callback   = "delete_from_my_visit_success";
+    generate_query(tmp_query, tmp_callback);
 }
