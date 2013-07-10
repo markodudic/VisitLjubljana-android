8f6362f9be33ab9c32f54ddf27fa530e3b9ee264
Marko Dudić <marko.dudic@gmail.com>
2013-07-10 20:10:55 +0000

    //audio guide logika

--- android/assets/www/assets/js/custom/guide_buy.js
+++ android/assets/www/assets/js/custom/guide_buy.js
@@ -1,18 +1,49 @@
-   			
+var audio_guides;
+var lang_has_purchased = 0;
+var lang_has_stored    = 0;
 
-function load_guide_buy() {
-	voice_guide = 1; 
-	
-	//ce je kupljen za ta jezik odpri
-	load_page(template_lang+'trips.html', 'trips', trips[4], 'fade', false, 4);
-	
-	//drugače
-	//load_page(template_lang+'guide_buy.html', 'guide_buy', trips[4], 'fade', false);
+function read_audio_guides_settings() {
+	var stored_audio_guides = window.localStorage.getItem("audio_guides");
+	if (stored_audio_guides == null) {
+		var defaults = {"1": {"purchased": "0", "stored": "1"},
+						"2": {"purchased": "0", "stored": "0"},
+						"3": {"purchased": "0", "stored": "0"},
+						"4": {"purchased": "0", "stored": "0"},
+						"5": {"purchased": "0", "stored": "0"}};
+		localStorage.setItem('audio_guides', JSON.stringify(defaults));
+		audio_guides = defaults;
+	} else {
+		audio_guides = $.parseJSON(stored_audio_guides);
+	}
 }
 
-function buy_guide() {
-	//nakup vodiča in synhronizacija
+function load_guide_buy() {
+	read_audio_guides_settings();
+	console.log(JSON.stringify(audio_guides[settings.id_lang]));
+	lang_has_purchased = audio_guides[settings.id_lang].purchased;
+	lang_has_stored    = audio_guides[settings.id_lang].stored;
 	
-	//na koncu prikaz
+	if (lang_has_purchased == 1 && lang_has_stored == 1) {
+		load_page(template_lang+'trips.html', 'trips', trips[4], 'fade', false, 4);
+	} else {
+		load_page(template_lang+'guide_buy.html', 'guide_buy', trips[4], 'fade', false);		
+	}
+}
+
+//klik na gumb
+function buy_guide() {
+	//recimo da smo kupili, za vsak jezik svoj sku
+	
+	//sinhronizacija v update.js:644
+	update_audio();
+	
+	//shranimo
+	lang_has_purchased = 1;
+	lang_has_stored    = 1;
+	audio_guides[settings.id_lang]["purchased"] = lang_has_purchased;
+	audio_guides[settings.id_lang]["stored"]    = lang_has_stored;
+	localStorage.setItem('audio_guides', JSON.stringify(audio_guides));
+	
+	//zrendramo na novo
 	load_guide_buy();
 }
