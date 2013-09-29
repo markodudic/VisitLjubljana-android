426ef96c6b00b6f745fc44c2f6466a058c3a1e66
Marko Dudić <marko.dudic@gmail.com>
2013-09-29 08:04:33 +0000

    upgrade

--- android/assets/www/assets/js/lib/datePickerPlugin.js
+++ android/assets/www/assets/js/lib/datePickerPlugin.js
@@ -1,64 +1,62 @@
 /**
- * Phonegap DatePicker Plugin Copyright (c) Greg Allen 2011 MIT Licensed
- * Reused and ported to Android plugin by Daniel van 't Oever
+ * Cordova DatePicker Plugin
+ * Copyright (c) Greg Allen 2011
+ * MIT Licensed
+ *
+ * Updated for Cordova 2.1 by Robert (Jamie) Munro
  */
-var DatePicker = (function (gap) {
-	/**
-	 * Constructor
-	 */
-	function DatePicker() {
-		this._callback;
-	}
-
-	/**
-	 * show - true to show the ad, false to hide the ad
-	 */
-	DatePicker.prototype.show = function(options, cb) {
-		if (options.date) {
-			options.date = (options.date.getMonth() + 1) + "/" + (options.date.getDate()) + "/" + (options.date.getFullYear()) + "/"
-					+ (options.date.getHours()) + "/" + (options.date.getMinutes());
-		}
-		var defaults = {
-			mode : '',
-			date : '',
-			allowOldDates : true
-		};
-
-		for ( var key in defaults) {
-			if (typeof options[key] !== "undefined")
-				defaults[key] = options[key];
-		}
-		this._callback = cb;
-
-		return gap.exec(cb, failureCallback, 'DatePickerPlugin', defaults.mode, new Array(defaults));
-	};
-
-	DatePicker.prototype._dateSelected = function(date) {
-		var d = new Date(parseFloat(date) * 1000);
-		if (this._callback)
-			this._callback(d);
-	};
-
-	function failureCallback(err) {
-		console.log("datePickerPlugin.js failed: " + err);
-	}
-
-	/**
-     * Load DatePicker
+if (typeof cordova !== "undefined") {
+    /**
+     * Constructor
      */
-    gap.addConstructor(function () {
-        if (gap.addPlugin) {
-            gap.addPlugin("datePicker", DatePicker);
-        } else {
-            if (!window.plugins) {
-                window.plugins = {};
-            }
+    function DatePicker() {
+        this._callback;
+    }
 
-            window.plugins.datePicker = new DatePicker();
+    /**
+     * show - true to show the ad, false to hide the ad
+     */
+    DatePicker.prototype.show = function(options, cb) {
+        var padDate = function(date) {
+          if (date.length == 1) {
+            return ("0" + date);
+          }
+          return date;
+        };
+
+        if (options.date) {
+            options.date = options.date.getFullYear() + "-" +
+                           padDate(options.date.getMonth()+1) + "-" +
+                           padDate(options.date.getDate()) +
+                           "T" + padDate(options.date.getHours()) + ":" +
+                           padDate(options.date.getMinutes()) + ":00Z";
         }
+        var defaults = {
+            mode : 'datetime',
+            date : '',
+            allowOldDates : true,
+            allowFutureDates : true
+        };
+
+        for (var key in defaults) {
+            if (typeof options[key] !== "undefined")
+                defaults[key] = options[key];
+        }
+        this._callback = cb;
+
+        cordova.exec("DatePicker.show", defaults);
+    };
+
+    DatePicker.prototype._dateSelected = function(date) {
+        var d = new Date(parseFloat(date) * 1000);
+        if (this._callback)
+            this._callback(d);
+    }
+
+    cordova.addConstructor(function() {
+        if (!window.plugins) {
+            window.plugins = {};
+        }
+        window.plugins.datePicker = new DatePicker();
     });
-	
-	return DatePicker;
-	
-	
-})(window.cordova || window.Cordova || window.PhoneGap);
+};
